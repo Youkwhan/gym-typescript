7cb9a5c3a9cb2102fe0876ca96d251303915537c
Youkwhan <Youkwhan@gmail.com>
2023-07-17 03:14:01 -0400

    Downgrade Prettier version for plugin compatibilty

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
 	return (
 		<>
-			<div className="pt-2 p-4">app</div>
+			<div className="ml-4 flex h-24 border-2 border-gray-300 p-3 pt-2 text-gray-700 shadow-md">
+				app
+			</div>
 			<div className="div"></div>
 		</>
 	)
