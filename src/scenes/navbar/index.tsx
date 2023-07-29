f9979577f9f9f8cb6a5801e7efe1b0fd49a06eab
Youkwhan <Youkwhan@gmail.com>
2023-07-29 00:22:16 -0400

    End: Finish Navbar Created selectedPage State in the parent component App.tsx

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react"
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png"
+import Link from "./Link"
 
-type Props = {}
+type Props = {
+	selectedPage: string
+	setSelectedPage: (value: string) => void
+}
 
-const Navbar = (props: Props) => {
+function Navbar({ selectedPage, setSelectedPage }: Props) {
 	// flexBetween a string that represent certain tailwind classes, reusable
 	const flexBetween = "flex items-center justify-between"
 
@@ -19,12 +23,28 @@ const Navbar = (props: Props) => {
 						{/*  RIGHT SIDE */}
 						<div className={`${flexBetween} w-full`}>
 							<div className={`${flexBetween} gap-8 text-sm`}>
-								<p>Home</p>
-								<p>About</p>
-								<p>Benefits</p>
-								<p>Our Classes</p>
-								<p>Contact Us</p>
+								<Link
+									page="Home"
+									selectedPage={selectedPage}
+									setSelectedPage={setSelectedPage}
+								/>
+								<Link
+									page="Benefits"
+									selectedPage={selectedPage}
+									setSelectedPage={setSelectedPage}
+								/>
+								<Link
+									page="Our Classes"
+									selectedPage={selectedPage}
+									setSelectedPage={setSelectedPage}
+								/>
+								<Link
+									page="Contact Us"
+									selectedPage={selectedPage}
+									setSelectedPage={setSelectedPage}
+								/>
 							</div>
+                     
 							<div className={`${flexBetween} gap-8`}>
 								<p>Sign In</p>
 								<button>Become a Member</button>
