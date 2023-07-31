b0f2d8db62fe4c5fec44f9a9932538550a2f9b70
Youkwhan <Youkwhan@gmail.com>
2023-07-30 22:33:45 -0400

    Navbar Complete

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -23,7 +23,7 @@ function Navbar({ isTopOfPage, selectedPage, setSelectedPage }: Props) {
 		<nav>
 			<div
 				className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
-			>
+			> 
 				<div className={`${flexBetween} mx-auto w-5/6`}>
 					<div className={`${flexBetween} w-full gap-16`}>
 						{/* LEFT SIDE */}
