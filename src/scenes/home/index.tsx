3203f556f74102de0c08ddcbb228d52a66aca5a4
Youkwhan <Youkwhan@gmail.com>
2023-08-03 20:27:41 -0400

    feat: Use onViewportEnter from motion.div
    for navbar navigation
    - Utilized onViewportEnter from motion.div to trigger a function upon entering the Home viewport.
    - Implemented setSelectedPage to dynamically highlight the navbar based on the current SelectedPage.

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -19,7 +19,10 @@ function Home({ setSelectedPage }: Props) {
 	return (
 		<section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
 			{/* IMAGE AND MAIN HEADER */}
-			<div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+			<motion.div
+				className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+				onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+			>
 				{/* MAIN HEADER */}
 				<div className="z-10 mt-32 md:basis-3/5">
 					{/* HEADINGS */}
@@ -75,7 +78,7 @@ function Home({ setSelectedPage }: Props) {
 				<div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
 					<img alt="home-pageGraphic" src={HomePageGraphic} />
 				</div>
-			</div>
+			</motion.div>
 
 			{/* SPONSORS */}
 			{isAboveMediumScreens && (
