e6d32656baaabfd17ece9591fc844820c01e6238
Youkwhan <Youkwhan@gmail.com>
2023-08-03 20:22:23 -0400

    feat: Framer motion on Actions section Staggering animation with delay to provide a more fluid transition.

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -47,7 +47,17 @@ function Home({ setSelectedPage }: Props) {
 					</motion.div>
 
 					{/* ACTIONS */}
-					<div className="mt-8 flex items-center gap-8">
+					<motion.div
+						className="mt-8 flex items-center gap-8"
+						initial="hidden"
+						whileInView="visible"
+						viewport={{ once: true, amount: 0.5 }}
+						transition={{ delay: 0.2, duration: 0.5 }}
+						variants={{
+							hidden: { opacity: 0, x: -50 },
+							visible: { opacity: 1, x: 0 },
+						}}
+					>
 						<ActionButton setSelectedPage={setSelectedPage}>
 							Join Now
 						</ActionButton>
@@ -58,7 +68,7 @@ function Home({ setSelectedPage }: Props) {
 						>
 							<p>Learn More</p>
 						</AnchorLink>
-					</div>
+					</motion.div>
 				</div>
 
 				{/* IMAGE */}
