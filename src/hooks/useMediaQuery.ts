956bb51ce0c5127d1fd758c5142907603e4e1d8b
Youkwhan <Youkwhan@gmail.com>
2023-08-13 21:44:36 -0400

    Refactor: useMediaQuery due to deprecated method

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,42 +1,17 @@
-import { useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 
-function useMediaQuery(query: string): boolean {
-	const getMatches = (query: string): boolean => {
-		// Prevents SSR issues
-		if (typeof window !== "undefined") {
-			return window.matchMedia(query).matches
-		}
-		return false
-	}
-
-	const [matches, setMatches] = useState<boolean>(getMatches(query))
-
-	function handleChange() {
-		setMatches(getMatches(query))
-	}
+const useMediaQuery = (query: string): boolean => {
+	const [matches, setMatches] = useState<boolean>(false)
 
 	useEffect(() => {
-		const matchMedia = window.matchMedia(query)
-
-		// Triggered at the first client-side load and if query changes
-		handleChange()
-
-		// Listen matchMedia
-		if (matchMedia.addListener) {
-			matchMedia.addListener(handleChange)
-		} else {
-			matchMedia.addEventListener("change", handleChange)
+		const media = window.matchMedia(query)
+		if (media.matches !== matches) {
+			setMatches(media.matches)
 		}
-
-		return () => {
-			if (matchMedia.removeListener) {
-				matchMedia.removeListener(handleChange)
-			} else {
-				matchMedia.removeEventListener("change", handleChange)
-			}
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [query])
+		const listener = () => setMatches(media.matches)
+		window.addEventListener("resize", listener)
+		return () => window.removeEventListener("resize", listener)
+	}, [matches, query])
 
 	return matches
 }
