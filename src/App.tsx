8b66e4226705cab54c8e68109c1f385a413b9702
Youkwhan <Youkwhan@gmail.com>
2023-08-17 20:13:59 -0400

    feat(Footer): Add footer component with info

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "@/scenes/home"
 import OurClasses from "@/scenes/ourClasses"
 import Benefits from "@/scenes/benefits"
 import ContactUs from "@/scenes/contactUs"
+import Footer from "@/scenes/footer"
 import { useState, useEffect } from "react"
 import { SelectedPage } from "@/shared/types"
 
@@ -40,6 +41,7 @@ function App() {
 			<Benefits setSelectedPage={setSelectedPage} />
 			<OurClasses setSelectedPage={setSelectedPage} />
 			<ContactUs setSelectedPage={setSelectedPage} />
+			<Footer />
 		</div>
 	)
 }
